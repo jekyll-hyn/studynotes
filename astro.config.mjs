1554ccdba73fbac05f715506b2dc2fa903a8e575
Huang <jekyllhyn@gmail.com>
2026-01-22 09:41:44 +0900

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   src/pages/blog/[...slug].astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://studynotes.jekyllhyn.workers.dev/",
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
-	},
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://studynotes.jekyllhyn.workers.dev/",
+  integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
