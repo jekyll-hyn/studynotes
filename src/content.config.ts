eb90593c875d902a30772bb89ee2d20dd224595c
Huang <jekyllhyn@gmail.com>
2026-01-22 10:38:11 +0900

    Changes to be committed: 	deleted:    public/blog-placeholder-2.jpg 	deleted:    public/blog-placeholder-3.jpg 	deleted:    public/blog-placeholder-4.jpg 	deleted:    public/blog-placeholder-5.jpg 	deleted:    public/blog-placeholder-about.jpg 	new file:   public/favicon.ico 	deleted:    public/favicon.svg 	renamed:    src/backup2026/BaseHead_2026.astro -> src/backup2026/BaseHead_2026.astro.txt 	renamed:    src/backup2026/BlogPost.astro -> src/backup2026/BlogPost.astro.txt 	renamed:    src/backup2026/Footer_2026.astro -> src/backup2026/Footer_2026.astro.txt 	renamed:    src/backup2026/Header_2026.astro -> src/backup2026/Header_2026.astro.txt 	renamed:    src/backup2026/about_2026.astro -> src/backup2026/about_2026.astro.txt 	renamed:    src/backup2026/index_2026.astro -> src/backup2026/index_2026.astro.txt 	renamed:    src/backup2026/indexmain_2026.astro -> src/backup2026/indexmain_2026.astro.txt 	modified:   src/components/BaseHead.astro 	modified:   src/consts.ts 	modified:   src/content.config.ts 	modified:   src/layouts/BaseLayout2p.astro 	modified:   src/layouts/BaseLayout3p.astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,7 +7,7 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string().optional(),
+		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
